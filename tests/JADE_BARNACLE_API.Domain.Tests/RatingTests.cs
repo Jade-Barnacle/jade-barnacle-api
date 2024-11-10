a226c37c27ec5456994d1586a571374f487bd535
Monesha <Monesharcrisp@yahoo.com>
2024-11-10 11:09:50 -0500

    done with page 7

--- tests/JADE_BARNACLE_API.Domain.Tests/RatingTests.cs
+++ tests/JADE_BARNACLE_API.Domain.Tests/RatingTests.cs
@@ -19,3 +19,40 @@ public class RatingTests
         Assert.AreEqual("Great fit!", rating.Review);
     }
 }
+
+[TestClass]
+    public class ItemTests
+    {
+        [TestMethod]
+        public void Can_Create_New_Item()
+        {
+            // Arrange
+            var item = new Item("Name", "Description", "Brand", 10.00m);
+
+            // Act (no actions needed for this test)
+
+            // Assert
+            Assert.AreEqual("Name", item.Name);
+            Assert.AreEqual("Description", item.Description);
+            Assert.AreEqual("Brand", item.Brand);
+            Assert.AreEqual(10.00m, item.Price);
+        }
+    }
+
+[TestClass]
+public class ItemTests
+{
+    [TestMethod]
+    public void Can_Create_Add_Rating()
+    {
+        // Arrange
+        var item = new Item("Name", "Description", "Brand", 10.00m);
+        var rating = new Rating(5, "Name", "Review");
+
+        // Act
+        item.AddRating(rating);
+
+        // Assert
+        Assert.AreEqual(rating, item.Ratings[0]);
+    }
+}
