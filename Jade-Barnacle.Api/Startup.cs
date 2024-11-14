5335c5e3bae27b174c967b514223c18f1fc56af0
Amal Ahmed <amvl.xxo@gmail.com>
2024-11-14 18:32:10 -0500

    Finished Add CORS Support. Pick up on pg 7.

--- Jade-Barnacle.Api/Startup.cs
+++ Jade-Barnacle.Api/Startup.cs
@@ -5,40 +5,58 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Jade.Barnacle.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Build.Framework;
+using Microsoft.Extensions.Options;
+
+
 namespace jade.barnacle.Api
 {
-public class Startup
-{
-public Startup(IConfiguration configuration)
-{
-Configuration = configuration;
-}
-public IConfiguration Configuration { get; }
-public void ConfigureServices(IServiceCollection services)
-{
-services.AddControllers();
-services.AddEndpointsApiExplorer();
-services.AddSwaggerGen();
-services.AddDbContext<StoreContext>(options =>
-    options.UseSqlite("Data Source =../Registrar.sqlite",
-        b => b.MigrationsAssembly("Jade-Barnacle.Api")));
-}
-public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-{
-if (env.IsDevelopment())
-{
-app.UseDeveloperExceptionPage();
-app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
-"jade.barnacle.Api v1"));
-}
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthorization();
-app.UseEndpoints(endpoints =>
-{
-endpoints.MapControllers();
-});
-}
-}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<StoreContext>(options =>
+                options.UseSqlite("Data Source =../Registrar.sqlite",
+                    b => b.MigrationsAssembly("Jade-Barnacle.Api")));
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+            services.AddEndpointsApiExplorer();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                "jade.barnacle.Api v1"));
+            }
+            app.UseHttpsRedirection();
+            app.UseCors();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
